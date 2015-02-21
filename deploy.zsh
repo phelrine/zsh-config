a11812b8ba5901296a1e0e9a5c7793c6b0806386
phelrine <phelrine@gmail.com>
2015-02-22 01:59:51 +0900

    rbenvの設定追加

--- deploy.zsh
+++ deploy.zsh
@@ -4,12 +4,10 @@ ANTIGEN_HOME="${HOME}/.antigen"
 ZPREZTO_HOME="${HOME}/.zprezto"
 
 if [ ! -e ${ANTIGEN_HOME} ]; then
-    echo "Installing antigen"
     git clone https://github.com/zsh-users/antigen.git ${ANTIGEN_HOME}
 fi
 source ${ANTIGEN_HOME}/antigen.zsh
 
-echo "Installing prezto"
 antigen bundle sorin-ionescu/prezto
 ln -snf ${ANTIGEN_HOME}/repos/.zprezto ${ZPREZTO_HOME}
 ln -sf ${BASE_DIR}/zprezto ${HOME}/.zpreztorc
